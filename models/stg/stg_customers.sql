f3aef3761853746bcf62019dca344c3dde20e26c
Brian Ivan Cusuanto <brian.ic@outlook.com>
2025-10-03 09:58:49 +0000

    cast and reformat the cols

--- models/stg/stg_customers.sql
+++ models/stg/stg_customers.sql
@@ -1,21 +1,20 @@
-WITH src AS 
-(
-    SELECT *
-    FROM {{ source('banking_bronze', 'customers') }}
-    WHERE _PARTITIONDATE = '2025-10-02'
-)
-
 SELECT
-    customer_id,
-    full_name,
-    date_of_birth,
-    gender,
-    email,
-    phone_number,
-    address,
-    city, province,
-    country,
-    join_date,
-    customer_segment,
-    kyc_status
-FROM src
+    CAST(customer_id AS INTEGER) AS customer_id,
+    CAST(UPPER(full_name) AS STRING) AS full_name,
+    CAST(date_of_birth AS DATE) AS date_of_birth,
+
+    CAST(CASE 
+        WHEN gender = 'M' THEN 'MALE'
+        WHEN gender = 'F' THEN 'FEMALE'
+    END AS STRING) as gender,
+
+    CAST(LOWER(email) AS STRING) AS email,
+    CAST(phone_number AS STRING) AS phone_number,
+    CAST(UPPER(address) AS STRING) AS address,
+    CAST(UPPER(city) AS STRING) AS city, province,
+    CAST(UPPER(country) AS STRING) AS country,
+    CAST(join_date AS DATE) AS join_date,
+    CAST(UPPER(customer_segment) AS STRING) AS customer_segment,
+    CAST(UPPER(kyc_status) AS STRING) AS kyc_status
+FROM {{ source('banking_bronze', 'customers') }}
+WHERE _PARTITIONDATE = '2025-10-02'
