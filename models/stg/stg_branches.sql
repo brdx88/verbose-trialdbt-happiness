0aef5b63b69b3ffe740c335b8212cf6f3a7f049b
Brian Ivan Cusuanto <brian.ic@outlook.com>
2025-10-03 09:58:30 +0000

    cast and reformat the cols

--- models/stg/stg_branches.sql
+++ models/stg/stg_branches.sql
@@ -1,11 +1,11 @@
 SELECT
-    branch_id,
-    branch_name,
-    city,
-    province,
-    country,
-    manager_name,
-    open_date,
-    employee_count
+    CAST(branch_id AS INTEGER) AS branch_id,
+    CAST(UPPER(branch_name) AS STRING) AS branch_name,
+    CAST(UPPER(city) AS STRING) AS city,
+    CAST(UPPER(province) AS STRING) AS province,
+    CAST(UPPER(country) AS STRING) AS country,
+    CAST(UPPER(manager_name) AS STRING) AS manager_name,
+    CAST(open_date AS DATE) AS open_date,
+    CAST(employee_count AS INTEGER) AS employee_count
 FROM {{ source('banking_bronze', 'branches') }}
 WHERE _PARTITIONDATE = '2025-10-02'
