4fc1ad4f45677de5847a692de56b4f20d5bef971
Brian Ivan Cusuanto <brian.ic@outlook.com>
2025-10-06 08:23:22 +0000

    cast and reformatting string cols

--- models/stg/stg_payments.sql
+++ models/stg/stg_payments.sql
@@ -1,12 +1,12 @@
 SELECT
-    payment_id,
-    loan_id,
-    payment_date,
-    payment_amount,
-    principal_component,
-    interest_component,
-    penalty_component,
-    payment_method,
-    payment_status
+    CAST(payment_id AS ) AS payment_id,
+    CAST(loan_id AS ) AS loan_id,
+    CAST(payment_date AS ) AS payment_date,
+    CAST(payment_amount AS ) AS payment_amount,
+    CAST(principal_component AS ) AS principal_component,
+    CAST(interest_component AS ) AS interest_component,
+    CAST(penalty_component AS ) AS penalty_component,
+    CAST(payment_method AS ) AS payment_method,
+    CAST(payment_status AS ) AS payment_status
 FROM {{ source('banking_bronze', 'payments') }}
 WHERE _PARTITIONDATE = '2025-10-02'
