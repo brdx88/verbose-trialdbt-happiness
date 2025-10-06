542be6109ad72b24ad39f28166e2fb659419d9b2
Brian Ivan Cusuanto <brian.ic@outlook.com>
2025-10-06 08:28:57 +0000

    enhance each cols to use proper dtype

--- models/stg/stg_payments.sql
+++ models/stg/stg_payments.sql
@@ -1,12 +1,12 @@
 SELECT
-    CAST(payment_id AS ) AS payment_id,
-    CAST(loan_id AS ) AS loan_id,
-    CAST(payment_date AS ) AS payment_date,
-    CAST(payment_amount AS ) AS payment_amount,
-    CAST(principal_component AS ) AS principal_component,
-    CAST(interest_component AS ) AS interest_component,
-    CAST(penalty_component AS ) AS penalty_component,
-    CAST(payment_method AS ) AS payment_method,
-    CAST(payment_status AS ) AS payment_status
+    CAST(payment_id AS INTEGER) AS payment_id,
+    CAST(loan_id AS INTEGER) AS loan_id,
+    CAST(payment_date AS DATE) AS payment_date,
+    CAST(payment_amount AS INTEGER) AS payment_amount,
+    CAST(principal_component AS INTEGER) AS principal_component,
+    CAST(interest_component AS INTEGER) AS interest_component,
+    CAST(penalty_component AS INTEGER) AS penalty_component,
+    CAST(UPPER(payment_method) AS STRING) AS payment_method,
+    CAST(UPPER(payment_status) AS STRING) AS payment_status
 FROM {{ source('banking_bronze', 'payments') }}
 WHERE _PARTITIONDATE = '2025-10-02'
